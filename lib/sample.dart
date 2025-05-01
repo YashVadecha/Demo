671ef37483cfed20b3c11773d38bae5a880779fa
Y THAKOR <ypthakor77@gmail.com>
2025-05-01 22:08:23 +0530

    add a simple page

---
+++ lib/sample.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class sample extends StatefulWidget {
+  const sample({super.key});
+
+  @override
+  State<sample> createState() => _sampleState();
+}
+
+class _sampleState extends State<sample> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
